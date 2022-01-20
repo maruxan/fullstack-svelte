f26e8849c6149232f53e8500af5193376e6b75c3
Mauricio Garavaglia <garavagliamauricio@gmail.com>
2022-01-20 13:33:47 -0300

    Add PATCH todo text

--- src/routes/api/todos/[uid].json.ts
+++ src/routes/api/todos/[uid].json.ts
@@ -3,3 +3,8 @@ import type { RequestHandler } from '@sveltejs/kit'
 import { api } from './_api'
 
 export const del: RequestHandler = (req) => api(req)
+
+export const patch: RequestHandler<unknown, FormData> = (req) => {
+  const data = { text: req.body.get('text') }
+  return api(req, data)
+}
