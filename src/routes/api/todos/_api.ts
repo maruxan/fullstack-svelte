fca662cec66fc73637a701c30b37ceb0e584c6b1
Mauricio Garavaglia <garavagliamauricio@gmail.com>
2022-01-26 11:43:12 -0300

    Adapt API to use Prisma client

--- src/routes/api/todos/_api.ts
+++ src/routes/api/todos/_api.ts
@@ -1,40 +1,41 @@
 import type { Request } from '@sveltejs/kit'
+import PrismaClient from '$lib/client/prisma'
 
-let todos: Todo[] = []
+const prisma = new PrismaClient()
 
-export const api = (req: Request, data?: Record<string, unknown>) => {
+export const api = async (req: Request, data?: Record<string, unknown>) => {
   let status = 500 // defaults to internal server error
   let body = {}
 
   switch (req.method.toUpperCase()) {
     case 'GET':
       status = 200
-      body = todos
+      body = await prisma.todo.findMany()
       break
 
     case 'POST':
-      todos.push(data as Todo)
+      body = await prisma.todo.create({ data: data as Todo })
       status = 201
-      body = data
       break
 
     case 'DELETE':
-      todos = todos.filter((todo) => todo.uid !== req.params.uid)
+      body = await prisma.todo.delete({
+        where: {
+          uid: req.params.uid,
+        },
+      })
       status = 200
-      body = { deletedUserId: req.params.uid.toString() }
       break
 
     case 'PATCH':
-      todos = todos.map((todo) => {
-        if (todo.uid === req.params.uid) {
-          // Either updates the todo text or done status
-          data.text
-            ? (todo.text = data.text as string)
-            : (todo.done = data.done as boolean)
-
-          body = todo
-        }
-        return todo
+      body = await prisma.todo.update({
+        where: {
+          uid: req.params.uid,
+        },
+        data: {
+          done: data.done,
+          text: data.text,
+        },
       })
       status = 200
       break
